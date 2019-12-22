d850f68b4fde49ca08299e333b1ad8c2960746dd
Paul Luna <paulluna0215@gmail.com>
2019-12-22 11:29:53 -0800

    got javascript working

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
+let board = new Board();
+
 const canvas = document.getElementById('board');
 const ctx = canvas.getContext('2d');
 
 // Calculate size of canvas from constants
-ctx.canvas.width = COLS * BlOCK_SIZE;
+ctx.canvas.width = COLS * BLOCK_SIZE;
 ctx.canvas.height = ROWS * BLOCK_SIZE;
 
 // Scale blocks
 ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
+
+function play() {
+    board.reset();
+    console.table(board.grid);
+}
